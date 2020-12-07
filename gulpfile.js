cd29dc1e5344ce7817e3b92b34ed78905e9ec6a5
Sophie Åkesson <sophie.akesson@gmail.com>
2020-12-07 08:56:47 +0100

    Add dependencies and README.md

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let watch = require("gulp-watch");
+var rename = require("gulp-rename");
+let minify = require("gulp-csso");
+
+gulp.task("default", function (){
+    return watch("scss/*.scss", function () {
+    gulp.src("scss/*.scss")
+    .pipe(sass())
+    .pipe(minify())
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest("css"))
+    });
+});
