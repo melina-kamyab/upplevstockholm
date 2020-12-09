4dc7deec6e162d2ad78b71d2bca95998394b707e
marv-ink <marvin96k@gmail.com>
2020-12-09 13:30:16 +0100

    made class and constructor and added jsconfig.jason file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$ (function(){
+    console.log(cultureExperianceOne);
+})
+class Experience {
+    constructor(Id,Category,Subcategory,Image,Name,Description,Price) {
+        this.Id = Id;
+        this.Category = Category;
+        this.Subcategory = Subcategory;
+        this.Image = Image;
+        this.Name = Name;
+        this.Description = Description;
+        this.Price = Price;
+  }
+}
